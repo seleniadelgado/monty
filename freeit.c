7ac70be21f85cb2ebed400d9f6a0d6004f284069
--get <angiedelgado08@gmail.com>
2018-12-19 22:35:16 +0000

    freeit.c

---
+++ freeit.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+ * freeit - frees the entire allocated memory.
+ * @stack: stack to be freed.
+ */
+void freeit(stack_t stack**)
+{
+	stack_t *temp = *stack;
+	stack_t *storage;
+
+	while (temp != NULL)
+	{
+		storage = temp->next;
+		free(temp);
+		storage = temp;
+	}
+}
