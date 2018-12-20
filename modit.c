99ff0951280b9263c24fb46d38cdade0ab04c264
--get <angiedelgado08@gmail.com>
2018-12-20 20:45:07 +0000

    changes in function

--- modit.c
+++ modit.c
@@ -7,27 +7,24 @@
  */
 void mod(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
-	unsigned int first;
-	unsigned int second;
-	unsigned int result;
+	stack_t *temp = info.tail;
+	int first;
+	int second;
+	int result;
 	unsigned int i = line_number;
 
 	if (info.num_nodes < 2)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", i);
+		freeit(stack);
 		exit(EXIT_FAILURE);
-	}
-
-	while ((temp->next != NULL) && (temp->next != NULL))
-	{
-		temp = temp->next;
 	}
 	second = temp->prev->n;
 	first = temp->n;
 	if (first == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
+		freeit(stack);
 		exit(EXIT_FAILURE);
 	}
 	result = second % first;
